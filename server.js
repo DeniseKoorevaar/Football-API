0a58a3a2d30f26821edc9b7b4807538acf0540a9
denise koorevaar <denise.koorevaar@appliedtechnology.se>
2019-10-31 09:02:49 +0100

    Server setup

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const port = 1234;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let baseUrl = 'https://apiv2.apifootball.com/?action=get_leagues&country_id=';
+//let id = 100;
+let apiKey = '&APIkey=00622ffc4e658e817b2bb7784792054a440bfa0ed9d383619ff21f748dc9dcc3';
+
+app.get('/countries', (req, res) => {
+    fetch('https://apiv2.apifootball.com/?action=get_countries&APIkey=00622ffc4e658e817b2bb7784792054a440bfa0ed9d383619ff21f748dc9dcc3')
+    .then(response => response.json())
+    .then(data => res.send(JSON.stringify(data)))
+});
+
+app.get('/competitions/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+        fetch(baseUrl + id + apiKey)
+        .then(response => response.json())
+        .then(data => res.send(JSON.stringify(data)))
+    });
+
+// app.get('/teams/:id', (req, res) => {
+//     fetch('https://apiv2.apifootball.com/?action=get_teams&league_id=148&APIkey=00622ffc4e658e817b2bb7784792054a440bfa0ed9d383619ff21f748dc9dcc3')
+//     .then(response => response.json())
+//     .then(data => res.send(JSON.stringify(data)))
+// });
+
+// app.get('/players/name', (req, res) => {
+//     fetch('https://apiv2.apifootball.com/?action=get_players&player_name=ronaldo cristiano&APIkey=00622ffc4e658e817b2bb7784792054a440bfa0ed9d383619ff21f748dc9dcc3')
+//     .then(response => response.json())
+//     .then(data => res.send(JSON.stringify(data)))
+// });
+
+app.get('/standings/:id', (req, res) => {
+    fetch('https://apiv2.apifootball.com/?action=get_standings&league_id=148&APIkey=00622ffc4e658e817b2bb7784792054a440bfa0ed9d383619ff21f748dc9dcc3')
+    .then(response => response.json())
+    .then(data => res.send(JSON.stringify(data)))
+});
+
+
+app.listen(port, () => console.log('Server listening on port ' + port));
