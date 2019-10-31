bf87b7b9478bf48599ce5e1d3bbe40bfb71a72c0
denise koorevaar <denise.koorevaar@appliedtechnology.se>
2019-10-31 09:54:12 +0100

    Leagues on click Country

--- public/index.js
+++ public/index.js
@@ -11,22 +11,29 @@ $(function () {
                 res.map(function (country) {
                     if (value === country.country_name) {
                         countryId = country.country_id;
-                        let p = document.createElement('p');
-                        p.classList.add('country');
+                        let p = document.getElementById('country');
                         let countryText = document.createTextNode(country.country_name);
                         p.appendChild(countryText);
-
-                        let bossDiv = document.querySelector('.container');
-                        bossDiv.appendChild(p);
-
-
-                        // p.addEventListener('click', getCountryId);
-                        p.onclick = () => {
-                            console.log(countryId);
-                        }
                     }
                 })
             }
         });
     });
 });
+
+$(function () {
+    $('#country').on('click', function () {
+        $.ajax({
+            url: '/competitions/:id',
+            type: 'GET',
+            dataType: 'json',
+            success: function (res) {
+                res.map(function (id) {
+                    console.log(id.league_name);
+    
+                })
+    
+            }
+        })
+    })
+})
